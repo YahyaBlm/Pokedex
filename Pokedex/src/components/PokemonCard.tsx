f1ba08243c9f8e4f2254f7639ddb4cd1b24a7827
YahyaBlm <yahya.belmou@gmail.com>
2025-01-21 15:23:20 +0100

    props.1

--- Pokedex/src/components/PokemonCard.tsx
+++ Pokedex/src/components/PokemonCard.tsx
@@ -1,15 +1,11 @@
 
-
-function PokemonCard() {
-    return(
-  <figure>
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-      alt=""
-    />
-    <figcaption>"Bulbasaur"</figcaption>
-  </figure>
-  )
+function PokemonCard({ pokemon }) {
+  return (
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
